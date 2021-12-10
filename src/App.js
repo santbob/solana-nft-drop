8ce219d81fb79c5f7bae9e24960b25a159426a56
Santhosh Nageshwar <santhosh_rao@intuit.com>
2021-12-09 23:39:15 -0800

    NFT's uploaded to arweave and Candy machine created and deployed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    const { solana } = window;
+    if (solana) {
+      if (solana.isPhantom) {
+        console.log('Phantom Wallet found');
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log('Connected with Public Key:', response.publicKey.toString());
+        // set the wallet address on the state variable
+        setWalletAddress(response.publicKey.toString());
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } else {
+      alert('Please connect to your wallet');
+    }
+  }
+
+  const connectWallet = async () => {
+    const { solana } = window;
+    if (solana) {
+      if (solana.isPhantom) {
+        console.log('Phantom Wallet found');
+        const response = await solana.connect();
+        console.log('Connected with Public Key:', response.publicKey.toString());
+        // set the wallet address on the state variable
+        setWalletAddress(response.publicKey.toString());
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } else {
+      alert('Please connect to your wallet');
+    }
+  }
+
+  const renderNotConnected = () => (<button
+    className="cta-button connect-wallet-button"
+    onClick={connectWallet}
+  >
+    Connect to Wallet
+  </button>)
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {!walletAddress && renderNotConnected()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
